e92f5a2ac418aa3302f30f5131682a8d207fb785
bkunzhang <19164121+bkunzhang@users.noreply.github.com>
2019-03-21 17:56:04 +0800

    Create ConcurrentDateUtil.java

---
+++ src/ConcurrentDateUtil.java
@@ -0,0 +1,26 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by zhbk on 2019/3/21.
+ * 线程安全的时间工具类
+ * SimpleDateFormat
+ */
+public class ConcurrentDateUtil {
+
+    public final static ThreadLocal<SimpleDateFormat> threadDateFormat = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    public static String format(Date date) {
+        return threadDateFormat.get().format(date);
+    }
+
+    public static Date parse(String dateStr) throws ParseException {
+        return threadDateFormat.get().parse(dateStr);
+    }
+}
