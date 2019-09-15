641f0dd2c48d01823edca9dba6e84eebc3aa67be
bkunzhang <19164121+bkunzhang@users.noreply.github.com>
2019-09-15 13:20:30 +0800

    LogUtil

---
+++ src/main/java/com/bkunzhang/util/LogUtil.java
@@ -0,0 +1,16 @@
+package com.bkunzhang.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * @author bkunzhang
+ * @date 2019/9/15
+ */
+public class LogUtil {
+    public static String getStackTraceInfo(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+}
